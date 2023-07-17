21a43e1233d5716de5eec753075b53dcb96e7d68
Yury <yury.katko@walgreens.com>
2023-07-17 10:14:25 -0400

    implement delete for encounter

--- pages/api/encounter/[id].js
+++ pages/api/encounter/[id].js
@@ -1,4 +1,4 @@
-import { getDocumentById } from "../../../lib/mongodb";
+import { getDocumentById, deleteDocument } from "../../../lib/mongodb";
 
 /**
  * @swagger
@@ -19,10 +19,47 @@ import { getDocumentById } from "../../../lib/mongodb";
  *       200:
  *         description: Returns a single Encounter entry
  */
-export default async function handler(req, res) {
+async function handleGet(req, res) {
   const query = req.query;
   const { id } = query;
   const result = await getDocumentById("Encounter", id);
-  
+
   res.status(200).json(result ?? "not found");
+}
+
+/**
+ * @swagger
+ * /api/encounter/{id}:
+ *   delete:
+ *     description: Deletes a single Encounter
+ *     tags:
+ *       - Encounter
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         default: 00000000-1111-2222-3333-444444444444
+ *         description: String ID (within resource.id) of the Encounter to delete
+ *     responses:
+ *       200:
+ *         description: Deletes a single Encounter
+ */
+async function handleDelete(req, res) {
+  const query = req.query;
+  const { id } = query;
+  const result = await deleteDocument("Encounter", id);
+
+  res.status(200).json(result);
+}
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    await handleGet(req, res);
+  }
+
+  if (req.method === "DELETE") {
+    await handleDelete(req, res);
+  }
 }
