f50337ebb90d053f52bf93b1c32324dbd0967d7d
Yury <yury.katko@walgreens.com>
2023-07-20 10:18:55 -0400

    better CORS

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://conversation-notes.vercel.app/" },
+          { key: "Access-Control-Allow-Origin", value: "https://conversation-notes.vercel.app" },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,POST,OPTIONS",
