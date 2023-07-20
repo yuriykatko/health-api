7c178db13124708a693478e5602fd5cf62bdaa06
Yury <yury.katko@walgreens.com>
2023-07-20 08:54:36 -0400

    add minimal POST for questionnaire

--- pages/api/questionnaire.js
+++ pages/api/questionnaire.js
@@ -11,8 +11,40 @@ import { getDocuments } from "../../lib/mongodb";
  *       200:
  *         description: Returns First 10 Questionnaire entries
  */
-export default async function handler(req, res) {
+async function handleGet(req, res) {
   const data = await getDocuments("Questionnaire", 10);
 
   res.status(200).json(data);
+}
+
+/**
+ * @swagger
+ * /api/questionnaire:
+ *   post:
+ *     description: Creates new Instance of Questionnaire
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *     tags:
+ *       - Questionnaire
+ *     responses:
+ *       200:
+ *         description: Creates new Instance of Questionnaire
+ */
+async function handlePost(req, res) {
+  const result = await addDocumentToCollection(req.body, "Questionnaire");
+  
+  res.status(200).json(result); 
+}
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    await handleGet(req, res);
+  }
+
+  if (req.method === "POST") {
+    await handlePost(req, res);
+  }
 }
