de016ab69b840b4fff19bd85e58d210efab869a4
Yury <yury.katko@walgreens.com>
2023-07-20 11:11:22 -0400

    will this work

--- pages/api/questionnaire.js
+++ pages/api/questionnaire.js
@@ -48,5 +48,7 @@ export default async function handler(req, res) {
     await handlePost(req, res);
   }
 
-  res.status(200).json("ok");
+  if (req.method === "OPTIONS") {
+    res.status(200).json("ok");
+  }
 }
