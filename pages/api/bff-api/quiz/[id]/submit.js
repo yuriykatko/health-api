c1632dfdf1f6595ea42ff5c8610a6892e237a327
Yury <yury.katko@walgreens.com>
2023-08-03 10:10:09 -0400

    update interface to include patient

--- pages/api/bff-api/quiz/[id]/submit.js
+++ pages/api/bff-api/quiz/[id]/submit.js
@@ -26,22 +26,29 @@ import { getResourceById, createResource } from "../../../../../lib/hapi";
  *         description: Submits Quiz response
  */
 async function handlePost(req, res) {
-    const query = req.query;
-    const { id } = query;
-    const fhirQuestionnaire = await getResourceById("Questionnaire", id);
-    const fhirQuestionnaireResponse = mapRJSFResponseToFHIR(req.body, id, fhirQuestionnaire);
-    
-    const fhirResponse = await createResource("QuestionnaireResponse", fhirQuestionnaireResponse);
+  const query = req.query;
+  const { id } = query;
+  const fhirQuestionnaire = await getResourceById("Questionnaire", id);
+  const fhirQuestionnaireResponse = mapRJSFResponseToFHIR(
+    req.body.quizResponse,
+    id,
+    fhirQuestionnaire
+  );
 
-    res.status(200).json(fhirResponse);
-  }
+  const fhirResponse = await createResource(
+    "QuestionnaireResponse",
+    fhirQuestionnaireResponse
+  );
+
+  res.status(200).json(fhirResponse);
+}
 
 export default async function handler(req, res) {
-    if (req.method === "POST") {
-      await handlePost(req, res);
-    }
-  
-    if (req.method === "OPTIONS") {
-      res.status(200).json("ok");
-    }
+  if (req.method === "POST") {
+    await handlePost(req, res);
   }
+
+  if (req.method === "OPTIONS") {
+    res.status(200).json("ok");
+  }
+}
